f404c2e40104d98b1bd151f43b923eab89193391
ReneKlever <rene.klever@gmail.com>
2025-04-13 14:20:13 +0200

    Update command_handlers.py

--- command_handlers.py
+++ command_handlers.py
@@ -184,7 +184,7 @@ def handle_bb_steps(sender_id, message, step, state, interface, bbs_nodes):
             if bulletins:
                 send_message(f"Select a bulletin number to view from {board_name} or press X to exit:", sender_id, interface)
                 for bulletin in bulletins:
-                    send_message(f"[{bulletin[0]}] {bulletin[1]} ({datum(bulletin[3])})", sender_id, interface)
+                    send_message(f"[{bulletin[0]}] {bulletin[1]} ({datum(bulletin[3])}) {bulletin[2]}", sender_id, interface)
                 update_user_state(sender_id, {'command': 'BULLETIN_READ', 'step': 3, 'board': board_name})
             else:
                 send_message(f"No bulletins in {board_name}.", sender_id, interface)
